2705be16c85815255e2df6a7a4de40a022481167
tahani <82546976+tahani95@users.noreply.github.com>
2021-06-06 15:36:31 +0300

    Update add remove element.js

--- AddRemoveElement/add remove element.js
+++ AddRemoveElement/add remove element.js
@@ -7,7 +7,7 @@ const button2 = document.getElementsByClassName('button2')[index-1];
 
 button1.addEventListener('click',() {
     index++;
-    elements.insertAdjacentHTML("beforeend",
+    elements.insertAdjacentHTML("before end",
     `<div class="element">
         <h3 >Element ${index}</h3>
         <h4>This is an element</h4>
@@ -15,12 +15,7 @@ button1.addEventListener('click',() {
     </div>
     `);
     
-})
+});
 
 
-    elements.addEventListener('click',(event) {
-    console.log(event.target.tagName)
-        if(event.target.tagName == "BUTTON"){
-        event.target.parentElement.remove();
-    };
-    });
+  
