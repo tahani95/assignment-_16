0367a84c5a64a12f866025bda6f3a9340b6eba9d
tahani <82546976+tahani95@users.noreply.github.com>
2021-06-06 15:40:44 +0300

    Update change element .js

--- ChangeElementStyle/change element .js
+++ ChangeElementStyle/change element .js
@@ -1,8 +1,26 @@
-<script>
-function disableBtn() {
-  document.getElementById("myBtn").disabled = true
-}
-function enableBtn() {
-  document.getElementById("myBtn").disabled = false
-}
-</script>
+const select = document.getElementsByTagName('select')[0];
+const submit = document.getElementById('submit');
+const content = document.getElementById('content');
+
+select.addEventListener('change',checkOptions);
+submit.addEventListener('click',changeColor);
+
+
+function checkOptions(event) {
+    event.preventDefault();
+    if (event.target.value !=""){
+        submit.classList.remove('disabled');
+    }
+    else {
+        content.className= `content `;
+        submit.classList.add('disabled');
+      function changeColor(event)
+{
+    const content = document.getElementById('content');
+    event.preventDefault();
+    
+    
+    if (select.value !== ""){
+        content.className= `content ` + select.value;
+    }
+    
