a3c6deb6acbaadb01fa9b9e5d4f53a4c59f9bd00
tahani <82546976+tahani95@users.noreply.github.com>
2021-06-06 15:31:24 +0300

    Update add remove element.js

--- AddRemoveElement/add remove element.js
+++ AddRemoveElement/add remove element.js
@@ -1,10 +1,26 @@
-<script>
-<Input id="elements" type="button" value="add Element"></Input>
-var element = document.getElementById("elem");
+
+ button1 = document.getElementById('add-element');
+ elements = document.getElementById('elements');
+let index = 0;
+let elementNumber = 0;
+const button2 = document.getElementsByClassName('button2')[index-1];
+
+button1.addEventListener('click',() {
+    index++;
+    elements.insertAdjacentHTML("beforeend",
+    `<div class="element">
+        <h3 >Element ${index}</h3>
+        <h4>This is an element</h4>
+        <button class="button2">Delete</button>
+    </div>
+    `);
     
-elements.onclick=function(){
-        alert (elements)};
-        <input type="button" id="elements" value="delte"> </input>
-        
-            elements.onclick=null()
-            </script>
+})
+
+
+    elements.addEventListener('click',(event) {
+    console.log(event.target.tagName)
+        if(event.target.tagName == "BUTTON"){
+        event.target.parentElement.remove();
+    };
+    });
